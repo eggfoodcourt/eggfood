c4c3c9f222f8015fe55cb46b19eeae68536a3102
eggfoodcourt <157870104+eggfoodcourt@users.noreply.github.com>
2024-01-27 11:08:19 +0000

    card align center

--- src/components/openingday/openingDay.tsx
+++ src/components/openingday/openingDay.tsx
@@ -10,38 +10,46 @@ interface Opening {
 const { openingImage, eggs } = Openindayimage;
 export const Openingday: FC = () => {
     return (
-        <Box
-            sx={{
-                minHeight: '50vh',
-                minWidth: '100%',
-                mt: { md: '0', lg: '0', sm: '20' },
-                display: 'flex',
-                justifyContent: 'center',
-                textAlign: 'left',
-                alignItems: 'center',
-                backgroundColor: 'background.default',
-                py: 5,
-            }}
-        >
-            <Box sx={{ backdropFilter: 'blur(20px)' }}>
-                <Grid container spacing={3}>
-                    {openingImage.map((Opening: Opening, index: number) => (
-                        <Grid item key={index} xs={12} sm={6} md={4} lg={3} spacing={6}>
-                            <Card sx={{ maxWidth: 400, borderRadius: 8,mt:5, boxShadow: '0 0 20px 1px rgba(251, 255, 254, 0.6)' }}>
-                                <CardActionArea>
-                                    <CardMedia
-                                        component="img"
-                                        height={200}
-                                        image={Opening.opening}
-                                        alt={"openingimages"}
-                                    />
-                                </CardActionArea>
-                            </Card>
-                        </Grid>
-                    ))}
-                </Grid>
-            </Box>
-        </Box>
+  
+    <Box
+    sx={{
+      minHeight: '100vh',
+      minWidth: '100%',
+      mt: { md: '0', lg: '0', sm: '20' },
+      backgroundPosition: 'center',
+      backgroundSize: 'cover',
+    //   backgroundImage: `url(${eggs})`,
+      display: 'flex',
+      justifyContent: 'center',
+      textAlign: 'left',
+      alignItems: 'center',
+      // opacity:0.1,
+      py: 5
+    }}
+  >
+    <Box >
+      <Container>
+        <Grid container spacing={3}>
+        {openingImage.map((Opening: Opening, index: number) => (
+            <Grid item key={index} xs={12} sm={6} md={4} lg={3}>
+              <Card sx={{ maxWidth: 450, borderRadius: 5 }}>
+                <CardActionArea>
+                  <CardMedia
+                    component="img"
+                    height="200"
+                    image={Opening.opening}
+                   alt={'openingimages'}
+                  />
+                </CardActionArea>
+              </Card>
+            </Grid>
+          ))}
+          
+
+        </Grid>
+      </Container>
+    </Box>
+  </Box>
     );
 };
 
