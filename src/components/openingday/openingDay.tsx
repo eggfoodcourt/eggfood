679ee6b041280ae6c55967105c19f72b2835d53b
eggfoodcourt <157870104+eggfoodcourt@users.noreply.github.com>
2024-01-27 09:48:16 +0000

    background

--- src/components/openingday/openingDay.tsx
+++ src/components/openingday/openingDay.tsx
@@ -7,7 +7,7 @@ interface Opening {
     id: string;
 }
 
-const { openingImage,eggs } = Openindayimage;
+const { openingImage, eggs } = Openindayimage;
 export const Openingday: FC = () => {
     return (
         <Box
@@ -15,30 +15,31 @@ export const Openingday: FC = () => {
                 minHeight: '50vh',
                 minWidth: '100%',
                 mt: { md: '0', lg: '0', sm: '20' },
-               display: 'flex',
+                display: 'flex',
                 justifyContent: 'center',
                 textAlign: 'left',
                 alignItems: 'center',
+                backgroundColor: 'secondary.main',
                 py: 5,
             }}
         >
             <Box sx={{ backdropFilter: 'blur(20px)' }}>
-            <Grid container spacing={3}>
-                        {openingImage.map((Opening: Opening, index: number) => (
-                            <Grid item key={index} xs={12} sm={6} md={4} lg={3}>
-                                <Card sx={{ maxWidth: 400, borderRadius: 8, boxShadow: '0 0 30px 5px rgba(72, 135, 202, 0.8)' }}>
-                                    <CardActionArea>
-                                        <CardMedia
-                                            component="img"
-                                            height={200}
-                                            image={Opening.opening}
-                                            alt={"openingimages"}
-                                        />
-                                    </CardActionArea>
-                                </Card>
-                            </Grid>
-                        ))}
-                    </Grid>
+                <Grid container spacing={3}>
+                    {openingImage.map((Opening: Opening, index: number) => (
+                        <Grid item key={index} xs={12} sm={6} md={4} lg={3}>
+                            <Card sx={{ maxWidth: 400, borderRadius: 8,mt:5, boxShadow: '0 0 40px 5px rgba(124, 54, 38, 0.8)' }}>
+                                <CardActionArea>
+                                    <CardMedia
+                                        component="img"
+                                        height={200}
+                                        image={Opening.opening}
+                                        alt={"openingimages"}
+                                    />
+                                </CardActionArea>
+                            </Card>
+                        </Grid>
+                    ))}
+                </Grid>
             </Box>
         </Box>
     );
